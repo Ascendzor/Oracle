43540292b2bfaeea19afdddbc05a836357fd0d86
ZacJ <zacharyzor@gmail.com>
2015-04-07 18:29:01 +1200

    node server running

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/derp', function (req, res) {
+	res.send('derp')
+	console.log('harrow')
+})
+
+var server = app.listen(1337, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
