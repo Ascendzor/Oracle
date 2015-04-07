df7a700b6d6054699f816bc6607b56dbaf83cb00
Ascendzor <wow.troymare@gmail.com>
2015-04-07 19:49:17 +1200

    clicked box being broadcasted

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-  
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 app.get('/', function (req, res) {
   res.sendFile('index.html', { root: __dirname + '/' })
 })
@@ -12,4 +13,12 @@ var server = app.listen(13337, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 
+});
+
+io = io.listen(server);
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('boxClicked', function (boxCoords) {
+	io.emit('boxClicked', boxCoords);
+  });
 });
